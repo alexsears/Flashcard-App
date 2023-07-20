3c33d305d747211c9ee27f68eadb724129cf602b
Alex Sears <54514645+alexsears@users.noreply.github.com>
2023-07-20 16:21:12 -0500

    Delete server/routes directory

--- server/routes/flashcardsRoutes.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const { getAllFlashcards } = require('../controllers/flashcardController');
-
-router.get('/', getAllFlashcards);
-
-module.exports = router;
--- server/routes/signupRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { postSignup } = require('../controllers/signupController');
-
-router.post('/', postSignup);
-
-module.exports = router;
