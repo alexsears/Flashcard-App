c8330cb3f1335f39e7f0c57e795ecbadb83e8c7a
Alex Sears <54514645+alexsears@users.noreply.github.com>
2023-07-20 16:20:22 -0500

    Delete reviewRoutes.js

--- routes/reviewRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { postReview } = require('../controllers/reviewController');
-
-router.post('/', postReview);
-
-module.exports = router;
